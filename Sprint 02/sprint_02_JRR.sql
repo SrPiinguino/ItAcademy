1fb367ae212b86d510a5861ad243f98ee14c7267
SrPiinguino <161459117+SrPiinguino@users.noreply.github.com>
2024-04-15 11:10:00 +0200

    Correciones

--- Sprint 02/sprint_02_JRR.sql
+++ Sprint 02/sprint_02_JRR.sql
@@ -1,72 +1,78 @@
-#Ex01
-select Alemania.*
-from (
-  select *
-  from transactions.transaction
-  where transactions.transaction.company_id in (
-    select transactions.company.id
-    from transactions.company
-    where transactions.company.country = "Germany"
-  )
-) as Alemania;
+#Nivel 1 - Ex01
+select *
+from transaction
+  where transaction.company_id in (
+    select id
+    from company
+    where company.country = "Germany"
+  );
 
-select distinct transaction.*, country
-from transactions.transaction
-join company on transaction.company_id = company.id
-where company.country= "Germany";
-
-#ex02
-select transactions.company.company_name, SUM(transactions.transaction.amount) as total_amount
-from transactions.transaction
-join transactions.company  ON transactions.transaction.company_id = transactions.company.id
-group by transactions.company.company_name
-having SUM(transactions.transaction.amount) > (select avg(amount) from transactions.transaction);
-
-#ex03
-Select transactions.company.company_name,transactions.transaction.*
-from transactions.transaction
-join  transactions.company  on transactions.transaction.company_id = transactions.company.id
-where transactions.company.company_name like "C%" or "c%" ;
-
-#ex04
-select transactions.company.company_name from transactions.company
- where not exists (select * from transactions.transaction
-                    where transactions.transaction.company_id = transactions.company.id);
-
-#Ex05
-select transaction.*, company.country
-from transactions.transaction
-join transactions.company on transaction.company_id = company.id
-where company.country in (
-    select country
-    from transactions.company
-    where company_name = 'Non Institute'
+#Nivel 1 - ex02
+SELECT company_name
+FROM company
+WHERE EXISTS (
+    SELECT 1
+    FROM transaction
+    WHERE transaction.company_id = company.id
+    AND transaction.amount > (SELECT AVG(amount) FROM transaction)
 );
 
+#Nivel 1 - ex03
+select company_name, t.* 
+from transaction t, 
+(select id, company_name
+from company
+where company_name like "c%") as c
+where company_id = c.id;
 
+#Nivel 1 - ex04
+select company_name from company
+ where not exists (select * from transaction
+                    where company_id = company.id);                    
 
-#EX06
-select transactions.company.company_name
-from transactions.company
-join transactions.transaction on transactions.company.id = transactions.transaction.company_id
-group by transactions.company.company_name
-having sum(transactions.transaction.amount) = (
-    select MAX(total_amount)
-    from (
-        select sum(transactions.transaction.amount) as total_amount
-        from transactions.transaction
-        group by transactions.transaction.company_id
-    ) AS subquery
+#Nivel 2 - ex01
+SELECT t.*, 
+       (SELECT country FROM company WHERE id = t.company_id) AS country
+FROM transaction t
+WHERE EXISTS (
+    SELECT 1 
+    FROM company c 
+    WHERE c.id = t.company_id 
+    AND c.country IN (
+        SELECT country 
+        FROM company 
+        WHERE company_name = 'Non Institute'
+    )
 );
 
-#Ex07
-select country
-from transactions.company
-join transactions.transaction on company.id = transaction.company_id
-group by country
-having count(transaction.id) > (select avg(recuento_id)
-from (select count(transaction.id) as recuento_id from transactions.transaction
-join transactions.company on transaction.company_id = company.id
-group by country) as sq);
+#Nivel 2 -ex02
+select c.company_name, valor
+from company c, (select company_id, max(amount) as valor
+from transaction
+group by company_id
+order by valor desc limit 1) as t
+where c.id = t.company_id
+;
 
+#Nivel 3 - ex01
+select country, avg(amount)as media
+from company, transaction
+where company.id= transaction.company_id
+group by country 
+having avg(amount)>(select avg(amount) 
+from transaction)
+;
+
+
+#Nivel 3 Ex02
+SELECT 
+    c.company_name,
+    CASE 
+        WHEN (SELECT COUNT(*) 
+              FROM transaction t 
+              WHERE t.company_id = c.id) > 4 THEN 'Más de 4 Transacciones'
+        ELSE 'Menos de 4 Transacciones'
+    END AS transaction_count
+FROM 
+    company c;
 
