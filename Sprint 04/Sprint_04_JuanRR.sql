49c7ea7b001c6a9195a39c4283a662a363ab0d50
SrPiinguino <161459117+SrPiinguino@users.noreply.github.com>
2024-04-18 14:51:01 +0200

    Subida Post Correcion German

--- Sprint 04/Sprint_04_JuanRR.sql
+++ Sprint 04/Sprint_04_JuanRR.sql
@@ -6,12 +6,13 @@ GROUP BY user_id
 HAVING count(transactions.id) >= 30
 );
 
+
 #Ex02
 #En este caso se suma las transacciones y se divide por el numero de iban aunque sean el mismo:
 SELECT  AVG(transactions.amount) as media_trans_iban_DonecLtd, iban
 FROM transactions
-JOIN credit_cards ON transactions.card_id = credit_cards.id
-JOIN companies ON transactions.business_id = companies.comapny_id
+JOIN credit_cards ON card_id = credit_cards.id
+JOIN companies ON business_id = companies.comapny_id
 WHERE companies.company_name= 'Donec Ltd'
 GROUP BY iban;
 
@@ -33,8 +34,8 @@ SELECT t.card_id,
                        WHERE t2.card_id = t.card_id 
                        ORDER BY t2.timestamp DESC 
                        LIMIT 3) AS last_three 
-                 WHERE declined = 1) = 3 THEN 0 ELSE 1 
-       END AS activada
+                 WHERE declined = 1) = 3 THEN "Desactivada" ELSE "Activada" 
+       END AS Estado_Tarjeta
 FROM transactions t
 GROUP BY t.card_id;
 
@@ -42,14 +43,44 @@ GROUP BY t.card_id;
 SELECT *
 FROM tarjetas_activadas;
 
-#ex 03
+#NIVEL 2 Ex 03
 SELECT COUNT(CARD_ID) AS CANTIDAD_TARJETAS_ACTIVAS
 FROM TARJETAS_ACTIVADAS
-WHERE ACTIVADA = 1;
+WHERE Estado_Tarjeta = "Activada";
 
-#La tabla de enlace fue creada en powerquery en el pdf esta explicado.
+
+#NIVEL 3
+/*La tabla de enlace fue creada en powerquery en el pdf esta explicado. sin embargo comentando en clase se me dijo 
+que era necesario hacerlo a traves de sql por eso he añadido el siguiente paso para hacerlo por ambos metodos
+*/
+##Primer Paso crear tabla de enlace:
+CREATE TABLE productos_transacciones_enlace (
+    transaction_id VARCHAR(255),
+    product_id INT
+);
+## Siguiente hacer una consulta para seleccionar las columnas y los datos que nos interesa
+
+INSERT INTO productos_transacciones_enlace (Transaction_ID, Product_ID) #Los valores seran introducidos en las columnas creadas de la nuva tabla
+SELECT id, SUBSTRING_INDEX(SUBSTRING_INDEX(Product_ids, ',', numbers.n), ',', -1) as Product_ID #Selecionamos el id de las transacciones y usamos la funcion substring index para poder buscar y separar las comas
+FROM Transactions
+JOIN (
+    SELECT 1 as n UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 #unimos las tablas generadas por indicices
+) numbers ON CHAR_LENGTH(Product_ids) - CHAR_LENGTH(REPLACE(Product_ids, ',', '')) >= numbers.n - 1; #Aqui restamos la longitud  real del registro menos la longitud del registro sin las comas para saber cuantas comas hay
+
+
+## Una vez creada la tabla establecemos las relaciones con las demas
+ALTER TABLE productos_transacciones_enlace ADD FOREIGN KEY (transaction_id) REFERENCES transactions(id);
+ALTER TABLE productos_transacciones_enlace ADD FOREIGN KEY (product_id) REFERENCES products(id);
+
+#Comprobamos la tabla
+SELECT *
+FROM productos_transacciones_enlace;
+##AÑADO COMO INTERES QUE TANTO LA TABLA CREADA CON POWER QUERY COMO ESTA TIENEN LOS MISMOS RESULTADOS:
+SELECT *
+FROM transactions_product;
+
 #Nivel 3 Ex01
 SELECT p.product_name, count(tp.product_id) as recuento_ventas
 FROM products p
-JOIN transactions_product tp ON p.id = tp.product_id
+JOIN productos_transacciones_enlace tp ON p.id = tp.product_id
 GROUP BY p.product_name;
